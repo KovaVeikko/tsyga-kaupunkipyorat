2ee98e3f80e821adea221c4ace282a5a3c363fe9
Veikko Kovanen <veikko.kovanen@gmail.com>
2018-03-25 17:32:10 +0300

    setup google maps for ios

--- src/modules/map/MapScreen.js
+++ src/modules/map/MapScreen.js
@@ -1,12 +1,41 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import MapView from 'react-native-maps/lib/components/MapView';
 
 const MapScreen = () => {
   return (
-    <View>
-      <Text>Map Screen</Text>
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        provider={MapView.PROVIDER_GOOGLE}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   )
 };
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  }
+});
 
 export default MapScreen;
